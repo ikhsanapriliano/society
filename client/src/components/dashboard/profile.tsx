7e8781ddf5cb8fc7e753b78c97c0231edee0ddac
ikhsan <ikhsanapriliano4@gmail.com>
2024-08-24 22:49:43 +0700

    add coming soon profile

--- client/src/components/dashboard/profile.tsx
+++ client/src/components/dashboard/profile.tsx
@@ -1,8 +1,8 @@
 const Profile = () => {
     return (
         <section className="p-5 w-full h-full">
-            <h2 className="font-semibold text-[20px]">Profile</h2>
-
+            <h2 className="font-semibold text-[20px] mb-5">Profile</h2>
+            <p>Coming soon.</p>
         </section>
     )
 }
