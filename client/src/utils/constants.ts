d12cfb451d9e3dafbf3e1ae4f1986245cd0f9f01
ikhsan <ikhsanapriliano4@gmail.com>
2024-08-27 16:43:48 +0700

    add env setting 2

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,9 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-export const wsUrl = process.env.NEXT_PUBLIC_WS_URL;
+const environment = process.env.NEXT_PUBLIC_ENVIRONMENT;
+export const apiUrl =
+    environment !== "development"
+        ? process.env.NEXT_PUBLIC_API_URL
+        : "http://localhost:3001";
+export const wsUrl =
+    environment !== "development"
+        ? process.env.NEXT_PUBLIC_WS_URL
+        : "ws://localhost:3001";
